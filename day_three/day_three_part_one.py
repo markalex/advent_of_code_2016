da6ffb758447ca271858435551eb5914f207275f
Mark Alexander <mark@simple.com>
2016-12-03 21:40:08 -0800

    updated

--- day_three/day_three_part_one.py
+++ day_three/day_three_part_one.py
@@ -5,8 +5,9 @@ from sys import stdin
 def process(input):
     count = 0
     for line in input.splitlines():
-        sides = sorted(map(int, [line[i:i+5] for i in range(0, len(line), 5)]))
-        if sides[0] + sides[1] > sides[2]:
+        sides = sorted([int(n) for n in line.split()])
+
+        if sum(sides[0:2]) > sides[2]:
             count += 1
 
     return count
--- day_three/day_three_part_two.py
+++ day_three/day_three_part_two.py
@@ -3,15 +3,16 @@ from sys import stdin
 
 
 def process(input):
-    count = 0
-    rows = [map(int, [line[i:i+5] for i in range(0, len(line), 5)])
-            for line in input.splitlines()]
+    rows = []
+
+    for line in input.splitlines():
+        rows.append([int(n) for n in line.split()])
 
     triangles = []
     for i in range(0, len(rows), 3):
         for j in range(3):
             triangles.append(sorted([rows[i][j], rows[i+1][j], rows[i+2][j]]))
 
-    return len([item for item in triangles if item[0] + item[1] > item[2]])
+    return len([item for item in triangles if sum(item[0:2]) > item[2]])
 
 print(process(stdin.read()))
