c8348101386d6d52647fb438fe2c4dcb8aacef83
Mark Alexander <mark@simple.com>
2016-12-06 13:14:46 -0800

    day_six

---
+++ day_six/day_six_part_two.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from sys import stdin
+import operator
+
+
+def process(input):
+    letters = []
+    word = []
+
+    for line in input.splitlines():
+        for i in range(len(line)):
+            c = line[i]
+            if len(letters) < len(line):
+                letters.append({c: 1})
+            else:
+                if c in letters[i]:
+                    letters[i][c] += 1
+                else:
+                    letters[i][c] = 1
+
+    for i in range(len(letters)):
+        word.append(
+            sorted(letters[i].items(),
+                   key=operator.itemgetter(1))[0][0])
+
+    return ''.join(word)
+
+
+print(process(stdin.read()))
