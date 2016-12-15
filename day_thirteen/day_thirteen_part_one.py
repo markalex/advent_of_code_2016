eebfff73b03eb7304b1fb1461d3ce3e85a763e07
Mark Alexander <mark@simple.com>
2016-12-14 18:07:23 -0800

    day thirteen

---
+++ day_thirteen/day_thirteen_part_one.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from sys import argv
+
+
+def process(coords, input):
+    visited = {(1, 1)}
+    current = {(1, 1)}
+    steps = 0
+
+    while True:
+        v = current.copy()
+        current = set()
+        for x, y in v:
+            for a, b in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                if a < 0 or b < 0:
+                    continue
+                if is_wall(a, b, input):
+                    continue
+                if (a, b) in visited:
+                    continue
+                visited.add((a, b))
+                current.add((a, b))
+        steps += 1
+        if coords in current:
+            return steps
+
+
+def is_wall(x, y, n):
+    calc = (x*x + 3*x + 2*x*y + y + y*y) + n
+    ones = bin(calc).count('1')
+    return (ones % 2) == 1
+
+
+print(process((int(argv[1]), int(argv[2])), int(argv[3])))
